80eb27f2dfb5a3904a4662adfc0da75e134df638
BrunoRubini <brunorubini06@gmail.com>
2025-04-16 21:03:54 -0300

    Se agregan cadenas de conexion para otros usuarios, momentaneamente

--- Server/Web/Program.cs
+++ Server/Web/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 #region Agregamos el servicio del DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(
-        builder.Configuration.GetConnectionString("BrunoConnectionLocal"),
+        builder.Configuration.GetConnectionString("BrunoConnectionLocal"), //ConnectionLocal (para valen y mateo)
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("BrunoConnectionLocal"))
     )
 );
