293cc8e56ba1899ef5377191b0a1b4c9bf2d33c4
JensonMedina <medinajenson74@gmail.com>
2025-05-02 19:55:37 -0300

    Se modifica el metodo getAll para usar usar la clase PagedResponse agregada por Valen 	new file:   Server/Application/Common/PagedResponse.cs 	modified:   Server/Application/Interfaces/IClienteService.cs 	deleted:    Server/Application/Models/Response/PagedResponse.cs 	modified:   Server/Application/Services/ClienteService.cs 	modified:   Server/Web/Controllers/ClienteController.cs

---
+++ Server/Application/Common/PagedResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.Common
+{
+    public class PagedResponse<T>
+    {
+        public List<T> Data { get; set; }
+        public int TotalItems { get; set; }         // Total de registros sin paginar
+        public int PageNumber { get; set; }         // Página actual
+        public int PageSize { get; set; }           // Cantidad de ítems por página
+        public int TotalPages =>
+            (int)Math.Ceiling((double)TotalItems / PageSize);  // Total de páginas
+
+        public bool HasPrevious => PageNumber > 1;
+        public bool HasNext => PageNumber < TotalPages;
+
+        public PagedResponse(List<T> data, int totalItems, int pageNumber, int pageSize)
+        {
+            Data = data;
+            TotalItems = totalItems;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
+
--- Server/Application/Interfaces/IClienteService.cs
+++ Server/Application/Interfaces/IClienteService.cs
@@ -1,11 +1,12 @@
-﻿using Application.Models.Request;
+﻿using Application.Common;
+using Application.Models.Request;
 using Application.Models.Response;
 
 namespace Application.Interfaces
 {
     public interface IClienteService
     {
-        Task<PagedResponse<ClienteResponse>> GetAll(int pageNumber, int pageSize, bool onlyActive = true, int NegocioId = 0);
+        Task<PagedResponse<ClienteResponse>> GetAll(int pageNumber, int pageSize, bool onlyActive = true, int negocioId = 0);
         Task<ClienteResponse> GetById(int id);
         Task Register(ClienteRequest request);
         Task Modify(int id, ClienteModifyRequest request);
